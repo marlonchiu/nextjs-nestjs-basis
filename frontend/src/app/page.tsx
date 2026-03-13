52941a26b73b8ad6ee206ba7c7b6b414764bf7d2
marlonchiu <Knight1898@outlook.com>
2026-03-13 15:15:09 +0800

    feat: 第一个表单页面

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-2xl text-blue-500">Root Page</p>
-      <Button>Button</Button>
-      <Button variant="secondary">Secondary</Button>
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">App Name</h1>
+        <Input name="name" placeholder="App Name"></Input>
+        <Textarea name="description" placeholder="App Description" />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   )
 }
